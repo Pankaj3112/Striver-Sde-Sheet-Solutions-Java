65351f81eebeea6941a36bb116db0e653a360933
Pankaj <pankajbeniwal3112@gmail.com>
2023-06-01 13:18:26 +0530

    added solution for pascal triangle

---
+++ Arrays-1/Pascal's-Triangle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> firstrow = new ArrayList<>();
+        firstrow.add(1);
+        ans.add(firstrow);
+
+
+        for(int i=1; i<numRows; i++){
+            List<Integer> inner = new ArrayList<>();
+            inner.add(1);
+
+            if(ans.get(i-1).size()>1){
+                for(int j=0; j<ans.get(i-1).size()-1; j++){
+                    inner.add(ans.get(i-1).get(j) + ans.get(i-1).get(j+1));
+                }
+            }
+
+            inner.add(1);
+
+            ans.add(inner);
+        }
+
+        return ans;
+
+    }
+}
