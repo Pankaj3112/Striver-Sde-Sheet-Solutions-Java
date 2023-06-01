ed5a5d07e9cc8472bf9ee5fda5a75b575bab4935
Pankaj <pankajbeniwal3112@gmail.com>
2023-06-01 13:57:30 +0530

    Kadane's Algo

---
+++ Arrays-1/Kadane's-Algo.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int max = Integer.MIN_VALUE;
+        int Currsum = 0;
+
+        for(int i=0; i<nums.length; i++){
+            Currsum+= nums[i];
+            max = Math.max(max, Currsum);
+            if(Currsum <0){
+                Currsum = 0;
+            }
+        }
+
+        return max;
+    }
+}
